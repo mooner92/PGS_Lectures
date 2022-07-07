0bd5880d59b232b8e5d444957c2063c9a98c6a59
Mooner92 <chlaudgjs0989@gmail.com>
2022-07-07 21:22:44 +0900

    add

--- ch11/unordered_set.cpp
+++ ch11/unordered_set.cpp
@@ -13,15 +13,14 @@ int main()
 	words.insert("radio");
 	words.insert("orange");
 	words.insert("ear");
-	
+
 	string word = "radio";
 	if (words.find(word) != words.end())
 		cout << word << " is used!" << endl;
 	else
 		cout << word << " is NOT used!" << endl;
 
-	vector<int> numbers {1, 5, 3, 1, 5, 7, 4, 5, 6, 3, 2, 7, 3, 6, 2};
+	vector<int> numbers{1, 5, 3, 1, 5, 7, 4, 5, 6, 3, 2, 7, 3, 6, 2};
 	unordered_set<int> num_set(numbers.begin(), numbers.end());
 	cout << num_set.size() << endl;
-
 }
